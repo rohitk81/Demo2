fb73f4878de09692e870ee9f5371d8f7b81b3c57
rohitk4jobs@gmail.com <rohitk4jobs@gmail.com>
2020-11-08 18:59:09 -0800

    Add project files.

---
+++ WebApplication1/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Course
+    {
+        public string Name;
+        public string Description;
+
+    }
+}
